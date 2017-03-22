9bef8790f1b3b3954d19dab16c3f6a37ec3ffaac
Kiara Ottogalli <kiara.ottogalli@gmail.com>
2017-03-22 10:47:20 +0100

    Unity project with 2 scripts: (1) CreateQuad.cs, a MonoBehaviour to create a simple quad mesh, add it to the object and render it. (2) CreatePrefab.cs, EditorWindow to create a menu item capable of adding a new prefab of a quad. Base for other posibilities like loading a file, extract the geometry an convert it to a unity prefab.

---
+++ Assets/CreatePrefab.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CreatePrefab : EditorWindow {
+
+	[MenuItem ("Examples/Create Prefab Quad")]
+	static void CreatePrefabQuad() {
+
+		// Create needed folders
+		if (!AssetDatabase.IsValidFolder ("Assets/Materials")) {
+			AssetDatabase.CreateFolder ("Assets", "Materials");
+			Debug.Log ("Materials folder created.");
+		}
+		if (!AssetDatabase.IsValidFolder ("Assets/Models")) {
+			AssetDatabase.CreateFolder ("Assets", "Models");
+			Debug.Log ("Models folder created.");
+		}
+		if (!AssetDatabase.IsValidFolder ("Assets/Prefabs")) {
+			AssetDatabase.CreateFolder ("Assets", "Prefabs");
+			Debug.Log ("Prefabs folder created.");
+		}
+
+		// For the labels of the Prefabs created
+		System.DateTime date = System.DateTime.Now;
+		string dateString = date.ToString ("yyyyMMddHHmmss");
+
+		// Create the mesh
+		Mesh mesh = new Mesh ();
+		Vector3[] vertices = new Vector3[] { new Vector3(1f, 1f, 0f), new Vector3(-1f, 1f, 0f), new Vector3(-1f, -1f, 0f), new Vector3(1f, -1f, 0f)};
+		int[] triangles = new int[] { 0, 2, 1, 0, 3, 2 };
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+		mesh.RecalculateNormals ();
+
+		// Save the mesh as an asset
+		string path = "Assets/Models/" + "mesh" + dateString + ".asset";
+		AssetDatabase.CreateAsset (mesh, path);
+
+		// Create the material
+		Material material = new Material(Shader.Find("Standard"));
+		// Save it as an asset
+		path = "Assets/Materials/" + "material" + dateString + ".mat";
+		AssetDatabase.CreateAsset (material, path);
+
+		// Create game object
+		GameObject gameObject = new GameObject("Quad" + dateString);
+		Debug.Log ("Created empty object named " + gameObject.name);
+
+		// Add the mesh filter component to the object,
+		// and assign the new mesh
+		gameObject.AddComponent<MeshFilter> ().mesh = mesh;
+		// Add the mesh renderer component to the object
+		// and assign the shader "Standard" so the material is Default-Material
+		gameObject.AddComponent<MeshRenderer> ().material = material;
+
+		// Save prefab
+		path = "Assets/Prefabs/" + gameObject.name + ".prefab";
+		Object prefab = PrefabUtility.CreateEmptyPrefab(path);
+		PrefabUtility.ReplacePrefab(gameObject, prefab, ReplacePrefabOptions.ConnectToPrefab);
+		DestroyImmediate (gameObject);
+	}
+}
---
+++ Assets/CreateQuad.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateQuad : MonoBehaviour {
+	void Start () {
+		// Create the mesh
+		Mesh mesh = new Mesh ();
+		Vector3[] vertices = new Vector3[] { new Vector3(1f, 1f, 0f), new Vector3(-1f, 1f, 0f), new Vector3(-1f, -1f, 0f), new Vector3(1f, -1f, 0f)};
+		int[] triangles = new int[] { 0, 2, 1, 0, 3, 2 };
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+		mesh.RecalculateNormals ();
+		// Add the mesh filter component to the object,
+		// and assign the new mesh
+		gameObject.AddComponent<MeshFilter> ().mesh = mesh;
+		// Add the mesh renderer component to the object
+		// and assign the shader "Standard" so the material is Default-Material
+		gameObject.AddComponent<MeshRenderer> ().material = new Material(Shader.Find("Standard"));
+	}
+}
